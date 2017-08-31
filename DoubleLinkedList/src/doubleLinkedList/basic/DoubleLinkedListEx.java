618dc05448e278260d7b98859d0f2505e38e822e
JunHee Lee <jhlee910609@gmail.com>
2017-08-31 21:04:42 +0900

    이중 연결리스트 예제 코드 추가

--- DoubleLinkedList/src/doubleLinkedList/basic/DoubleLinkedListEx.java
+++ DoubleLinkedList/src/doubleLinkedList/basic/DoubleLinkedListEx.java
@@ -1,10 +1,38 @@
 package doubleLinkedList.basic;
 
+import circularLinkedList.basic.CircularLinkedListEx.Node;
+
 public class DoubleLinkedListEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
+	
+	// 노드 클래스를 설계한다.
+		class Node {
+
+			// data field
+			public String data;
+
+			// link field
+			public Node next;
+
+			// 파라미터 경우의 수에 따른 다양한 생성자를 오버로드한다.
+			public Node() {
+				this.data = null;
+				this.next = null;
+			}
+
+			public Node(String data) {
+				this.data = data;
+				this.next = null;
+			}
+
+			public Node(String data, Node link) {
+				this.data = data;
+				this.next = link;
+			}
+		}
 
 }
